c7c9bbc99286c831da1d5c684d1632b18f0c7527
Vasileios Anagnostopoulos <vasilis.anagnostopoulos@agileactors.com>
2024-07-04 18:46:51 +0300

    feat: finalize tests

--- omdb_dbt_project/models/movie_info_silver/all_casts_cleansed.sql
+++ omdb_dbt_project/models/movie_info_silver/all_casts_cleansed.sql
@@ -8,6 +8,7 @@ with source_all_casts_cleansed as (
     distinct *
     
     FROM {{ref('all_casts_with_types')}}
+    where movie_id in (select movie_id from {{ref('with_cleansing_all_ids_of_video')}})
 )
 
 select * from source_all_casts_cleansed
