c0a9aad734ac3e8514d0092a38b221e73844ed90
Vasileios Anagnostopoulos <vasilis.anagnostopoulos@agileactors.com>
2024-07-05 11:36:30 +0300

    feat: refactor tests

--- omdb_dbt_project/models/movie_info_silver/all_casts_cleansed.sql
+++ omdb_dbt_project/models/movie_info_silver/all_casts_cleansed.sql
@@ -8,7 +8,7 @@ with source_all_casts_cleansed as (
     distinct *
     
     FROM {{ref('all_casts_with_types')}}
-    where movie_id in (select movie_id from {{ref('with_cleansing_all_ids_of_video')}})
+    where movie_id in (select movie_id from {{ref('with_cleansing_all_ids_of_original_movies_and_series')}})
 )
 
 select * from source_all_casts_cleansed
