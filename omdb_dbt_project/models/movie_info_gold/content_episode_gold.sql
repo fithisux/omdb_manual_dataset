c27a18a14ec2028721f7b450cfdf3acf9d8b334c
Vasileios Anagnostopoulos <vasilis.anagnostopoulos@agileactors.com>
2024-10-14 12:25:08 +0300

    feat: extra tests for episodes

--- omdb_dbt_project/models/movie_info_gold/content_episode_gold.sql
+++ omdb_dbt_project/models/movie_info_gold/content_episode_gold.sql
@@ -4,10 +4,23 @@ with source_content_episode_gold as (
 
     select
 
-    id, parent_id
+    id, series_id as provided_series_id, 
+    coalesce(parent_id, series_id) as series_id, 
+    null as season_id
     
-    FROM {{ ref('content_gold') }}
-    where content_type='episode'
+    FROM {{ ref('all_episodes_cleansed') }}
+    where parent_id is null or parent_id in (select id from {{ ref('all_series_cleansed') }})
+
+    UNION
+
+    select
+
+    aa.id as id, aa.series_id as provided_series_id, 
+    coalesce(bb.parent_id, aa.series_id) as series_id, 
+    aa.parent_id as season_id
+    
+    FROM {{ ref('all_episodes_cleansed') }} aa
+    inner join {{ ref('all_seasons_cleansed') }} bb on aa.parent_id=bb.id
 )
 
 select * from source_content_episode_gold
