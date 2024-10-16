cf29461c58ba718daffc5252bfa6e75db2a3bd14
Vasileios Anagnostopoulos <vasilis.anagnostopoulos@agileactors.com>
2024-10-16 13:50:09 +0300

    feat: reduce data

--- omdb_dbt_project/models/movie_info_gold/all_casts_gold.sql
+++ omdb_dbt_project/models/movie_info_gold/all_casts_gold.sql
@@ -5,9 +5,17 @@ with source_all_casts_gold as (
 
     select
 
-    distinct *
+    distinct movie_id, person_id, job_id, role
     
     FROM {{ref('all_casts_cleansed')}}
+), temp_result as (
+    select
+
+    movie_id, person_id, job_id, array_agg(role) as roles
+
+    from source_all_casts_gold
+
+    group by movie_id, person_id, job_id
 )
 
-select * from source_all_casts_gold
+select * from temp_result
