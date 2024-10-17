2edfb720d4780266de7c935e8bed6fd9d1f29b19
Vasileios Anagnostopoulos <vasilis.anagnostopoulos@agileactors.com>
2024-10-17 12:14:55 +0300

    feat: make abstract distinct

--- omdb_dbt_project/models/movie_info_silver/movie_abstracts_en_cleansed.sql
+++ omdb_dbt_project/models/movie_info_silver/movie_abstracts_en_cleansed.sql
@@ -4,11 +4,12 @@ with source_movie_abstracts_en_cleansed as (
 
     select
 
-    *
+    movie_id, string_agg(abstract, ' ;;; ') as abstract
     
     FROM {{ ref('movie_abstracts_en_with_types') }}
     where movie_id in (select movie_id from {{ref('with_cleansing_all_ids_of_original_movies_and_series')}})
 
+    group by movie_id
 )
 
 select * from source_movie_abstracts_en_cleansed
--- omdb_dbt_project/models/movie_info_silver/movie_abstracts_es_cleansed.sql
+++ omdb_dbt_project/models/movie_info_silver/movie_abstracts_es_cleansed.sql
@@ -4,10 +4,12 @@ with source_movie_abstracts_es_cleansed as (
 
     select
 
-    *
+    movie_id, string_agg(abstract, ' ;;; ') as abstract
     
     FROM {{ ref('movie_abstracts_es_with_types') }}
     where movie_id in (select movie_id from {{ref('with_cleansing_all_ids_of_original_movies_and_series')}})
+
+    group by movie_id
 
 )
 
