434be6a405be6e40f6b4a6360c57860f0c6152c2
Vasileios Anagnostopoulos <vasilis.anagnostopoulos@agileactors.com>
2024-10-21 15:19:12 +0300

    feat: make cloumns explicit

--- omdb_dbt_project/models/movie_info_graph/node_content.sql
+++ omdb_dbt_project/models/movie_info_graph/node_content.sql
@@ -2,7 +2,8 @@
 
 with source_content_nodes_gold as (
 
-    select *  from {{ ref('content_gold') }}
+    select content_id, name, date, content_type, runtime, revenue, budget, homepage, votes_count, vote_average, last_update 
+    from {{ ref('content_gold') }}
 
 )
 
