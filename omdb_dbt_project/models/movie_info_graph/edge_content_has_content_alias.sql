c64a9599f45f6dbd924979224a4a1d34c26905cf
Vasileios Anagnostopoulos <vasilis.anagnostopoulos@agileactors.com>
2024-10-21 09:45:21 +0300

    feat: refactors

--- omdb_dbt_project/models/movie_info_graph/edge_content_has_content_alias.sql
+++ omdb_dbt_project/models/movie_info_graph/edge_content_has_content_alias.sql
@@ -6,7 +6,7 @@ with source_has_alias_edges as (
 
     content_id as src_content_id, language_iso_639_1 as dst_language_iso_639_1, name as dst_name
     
-    FROM {{ ref('content_aliases_iso_gold') }}
+    FROM {{ ref('content_alias_gold') }}
 )
 
 select * from source_has_alias_edges
--- omdb_dbt_project/models/movie_info_graph/node_content_alias.sql
+++ omdb_dbt_project/models/movie_info_graph/node_content_alias.sql
@@ -6,7 +6,7 @@ with source_content_aliases_iso_nodes as (
 
     distinct language_iso_639_1, name, official_translation
     
-    FROM {{ ref('content_aliases_iso_gold') }}
+    FROM {{ ref('content_alias_gold') }}
 
 )
 
