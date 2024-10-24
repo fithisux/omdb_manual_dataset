38f8eefccdc55fee4d6fc5614ddf8898564d2bbf
Vasileios Anagnostopoulos <vasilis.anagnostopoulos@agileactors.com>
2024-10-24 19:32:30 +0300

    fix: fields

--- omdb_dbt_project/models/gold_business_graph/gold_business_graph_node_content.sql
+++ omdb_dbt_project/models/gold_business_graph/gold_business_graph_node_content.sql
@@ -2,7 +2,9 @@
 
 with source_content as (
 
-    select *
+    select content_id, parent_id, date, content_type, runtime, revenue, budget, 
+    homepage, vote_average, votes_count, last_update, countries, abstracts, content_links, person_links,
+    refs, trailers, aliasings
     FROM {{ref('gold_business_content')}}
 )
    
--- omdb_dbt_project/models/gold_normalized/gold_normalized_person.sql
+++ omdb_dbt_project/models/gold_normalized/gold_normalized_person.sql
@@ -5,7 +5,14 @@ with source_person as (
 
     select
 
-    id as person_id, name, birthday,deathday, gender
+    id as person_id, name, birthday, deathday, 
+    case 
+        when gender=0 then 'male' 
+        when gender=1 then 'female'
+        when gender=2 then 'nonbinary'
+        when gender=3 then 'animal'
+        else 'unspecified'
+    end as gender
     
     FROM {{ ref('silver_cleansed_all_people') }}
 ), person_aliases as (
