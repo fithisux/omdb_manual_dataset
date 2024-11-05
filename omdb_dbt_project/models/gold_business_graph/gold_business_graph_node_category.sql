dfe88ce32544967d70cbe524c592a3089b20d675
Vasileios Anagnostopoulos <vasilis.anagnostopoulos@agileactors.com>
2024-11-05 15:55:01 +0200

    fix: names and dead code

--- omdb_dbt_project/models/gold_business_graph/gold_business_graph_node_category.sql
+++ omdb_dbt_project/models/gold_business_graph/gold_business_graph_node_category.sql
@@ -2,13 +2,11 @@
 
 with source_category as (
 
-    select *
-    FROM {{ref('gold_business_category')}}
-), category_named as (
-    select category_id, array_agg(ROW(name, language_iso_639_1)::omdb_name_type) as names
-    FROM {{ref('gold_normalized_category_name')}}
-    group by category_id
+    select
+        category_id,
+        names
+    from {{ ref('gold_business_category') }}
 )
 
-select*
+select *
 from source_category
